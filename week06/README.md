f9160fbefccf5831114bcf3ded4207c9221b65d5
hxhowl <zcanfly@126.com>
2021-04-13 22:49:31 +0800

    week06作业
    week06作业

---
+++ week06/README.md
@@ -0,0 +1,9 @@
+# geekbang-java-task
+
+#### week06
+
+- my-cache 模块
+  - 提供一套抽象 API 实现对象的序列化和反序列化 通过 Lettuce 实现一套 Redis CacheManager 以及 Cache
+
+
+
---
+++ week06/user-platform/my-cache/src/main/java/org/geektimes/cache/redis/lettuce/codec/GenericRedisCodec.java
@@ -0,0 +1,100 @@
+package org.geektimes.cache.redis.lettuce.codec;
+
+import io.lettuce.core.codec.RedisCodec;
+import org.geektimes.cache.serializer.ByteArraySerializer;
+import org.geektimes.cache.serializer.CacheSerializer;
+
+import java.nio.ByteBuffer;
+
+public class GenericRedisCodec<K, V> implements RedisCodec<K, V> {
+
+    private static final byte[] EMPTY = new byte[0];
+    private final Codec keyCodec;
+    private final Codec valueCodec;
+    private final CacheSerializer<Object, byte[]> serializer;
+
+
+    public GenericRedisCodec(Class<K> k, Class<V> v) {
+        this.keyCodec = initCodec(k);
+        this.valueCodec = initCodec(v);
+        this.serializer = new ByteArraySerializer();
+    }
+
+    private Codec initCodec(Class clazz) {
+        if (clazz == null) {
+            throw new RuntimeException("not support types!");
+        } else if (clazz.equals(String.class)) {
+            return new StringCodec();
+        } else if (clazz.equals(Integer.class)) {
+            return new IntegerCodec();
+        } else if (clazz.equals(Byte.class)) {
+            return new ByteCodec();
+        } else if (clazz.equals(Short.class)) {
+            return new ShortCodec();
+        } else if (clazz.equals(Long.class)) {
+            return new LongCodec();
+        } else if (clazz.equals(Float.class)) {
+            return new FloatCodec();
+        } else if (clazz.equals(Double.class)) {
+            return new DoubleCodec();
+        } else {
+            return null;
+        }
+    }
+
+
+    @Override
+    public K decodeKey(ByteBuffer buffer) {
+        if (this.keyCodec != null) {
+            return (K) keyCodec.decode(buffer);
+        }
+        return (K) deCode(buffer);
+    }
+
+    @Override
+    public V decodeValue(ByteBuffer buffer) {
+        if (this.valueCodec != null) {
+            return (V) valueCodec.decode(buffer);
+        }
+        return (V) deCode(buffer);
+    }
+
+    private Object deCode(ByteBuffer buffer) {
+        int remaing = buffer.remaining();
+        if (buffer == null || remaing == 0) {
+            return null;
+        }
+        byte[] bytes = new byte[remaing];
+        buffer.get(bytes);
+        return serializer.deserialize(bytes);
+    }
+
+
+    private ByteBuffer encode(Object obj) {
+        if (obj == null) {
+            return ByteBuffer.wrap(EMPTY);
+        }
+        byte[] bytes = serializer.serialize(obj);
+        return ByteBuffer.wrap(bytes);
+    }
+
+
+    @Override
+    public ByteBuffer encodeKey(K key) {
+        if (this.keyCodec != null) {
+            return keyCodec.encode(key);
+        }
+        return encode(key);
+    }
+
+
+    @Override
+    public ByteBuffer encodeValue(V value) {
+        if (this.valueCodec != null) {
+            return valueCodec.encode(value);
+        }
+        return encode(value);
+    }
+
+
+}
